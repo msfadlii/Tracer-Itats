bcbe75cd7bb47b01c806e004e8db167228f0e766
msfadlii <msfadliii@gmail.com>
2026-01-22 16:28:10 +0700

    test lagi 3

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/alumni/export', [AlumniController::class, 'export'])->name('alumni.export');
-Route::get('/analytics/summary', [AnalyticsController::class, 'summary']);
 
 //Admin route group
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
